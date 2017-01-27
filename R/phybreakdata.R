db9724eeff3118fbae60ca3ea8bb868c8b4d0f1b
Sjors Stouten <stoutens@rl03-int-p.rivm.nl>
2017-01-27 11:56:04 +0100

    Now compatible with multiple genes

--- R/phybreakdata.R
+++ R/phybreakdata.R
@@ -49,22 +49,24 @@ phybreakdata <- function(sequences, sample.times, host.names = NULL, sim.infecti
   ##########################################################
   ### testing the input: first the essential information ###
   ##########################################################
-  if(!any(class(sequences) %in% c("DNAbin", "phyDat", "matrix"))) {
+  if(class(sequences) != "list" & !any(class(sequences) %in% c("DNAbin", "phyDat", "matrix"))) {
     stop("sequences should be of class \"DNAbin\", \"phyDat\", or \"character\"")
   }
+  if(class(sequences) == "list") {Ngenes <- length(sequences)} else {Ngenes <- 1}
   if(inherits(sequences, "matrix") && !inherits(sequences[1], "character")) {
     stop("sequences matrix should contain \"character\" elements")
   }
-  if(inherits(sequences, c("DNAbin"))) {
-    sequences <- phangorn::as.phyDat(sequences)
+  if(all(lapply(sequences,class) == "DNAbin")) {
+    Ngenes <- length(sequences)
+    sequences <- lapply(1:Ngenes, function(gene) phangorn::as.phyDat(sequences[[gene]]))
   }
-  if(inherits(sequences, c("phyDat"))) {
-    sequences <- as.character(sequences)
+  if(all(lapply(sequences,class) == "phyDat")) {
+    sequences <- lapply(1:Ngenes, function(gene) as.character(sequences[[gene]]))
   }
   if(!inherits(sample.times, c("Date", "numeric", "integer"))) {
     stop("sample.times should be numeric or of class \"Date\"")
   }
-  if(nrow(sequences) != length(sample.times)) {
+  if(all(lapply(sequences,nrow) != length(sample.times))) {
     stop("numbers of sequences and sample.times don't match")
   }
   if(!is.null(host.names)) {
@@ -79,16 +81,16 @@ phybreakdata <- function(sequences, sample.times, host.names = NULL, sim.infecti
       warning("names in sample.times don't match host.names and are therefore overwritten")
       names(sample.times) <- host.names
     }
-    if(is.null(row.names(sequences))) {
-      row.names(sequences) <- host.names
-    } else if (all(row.names(sequences) %in% host.names)) {
-      sequences <- sequences[host.names, ]
+    if(any(sapply(1:Ngenes, function(gene) is.null(row.names(sequences[[gene]]))))) {
+      for (gene in 1:Ngenes) { row.names(sequences[[gene]]) <- host.names }
+    } else if (any(row.names(sequences) %in% host.names)) {
+      for (gene in 1:Ngenes) {sequences <- sequences[[gene]][host.names, ] }
     } else {
       warning("names in sequences don't match host.names and are therefore overwritten")
-      row.names(sequences) <- host.names
+      for (gene in 1:Ngenes) {row.names(sequences[[gene]]) <- host.names}
     }
-  } else if(!is.null(row.names(sequences))) {
-    host.names <- row.names(sequences)
+  } else if(!any(sapply(1:Ngenes, function(gene) is.null(row.names(sequences[[gene]]))))) {
+    host.names <- row.names(sequences[[1]])
     if(is.null(names(sample.times))) {
       names(sample.times) <- host.names
     } else if (all(names(sample.times) %in% host.names)) {
@@ -99,24 +101,27 @@ phybreakdata <- function(sequences, sample.times, host.names = NULL, sim.infecti
     }
   } else if(!is.null(names(sample.times))) {
     host.names <- names(sample.times)
-    row.names(sequences) <- host.names
+    for (gene in 1:Ngenes) {row.names(sequences[[gene]]) <- host.names}
   } else {
     host.names <- paste0("host.", 1:length(sample.times))
-    row.names(sequences) <- host.names
+    for (gene in 1:Ngenes) {row.names(sequences[[gene]]) <- host.names}
     names(sample.times) <- host.names
   }
   
   #################################################
   ### place essential information in outputlist ###
   #################################################
-  if(length(setdiff(sequences,c("a","c","g","t")))) warning("all nucleotides other than actg are turned into n")
-  sequences[sequences != "a" & sequences != "c" & sequences != "g" & sequences != "t"] <- "n"
-  sequences <- phangorn::as.phyDat(sequences)
-  
-  res <- list(
-    sequences = sequences,
-    sample.times = sample.times
-  )
+  for (gene in 1:Ngenes) {
+    if(length(setdiff(sequences[[gene]],c("a","c","g","t")))) warning("all nucleotides other than actg are turned into n")
+    sequences[[gene]][sequences[[gene]] != "a" & sequences[[gene]] != "c" & 
+                        sequences[[gene]] != "g" & sequences[[gene]] != "t"] <- "n"
+    sequences[[gene]] <- phangorn::as.phyDat(sequences[[gene]])
+    
+    res <- list(
+      sequences = sequences,
+      sample.times = sample.times
+    )
+  }
   ######################################################################
   ### testing and adding the input: then the rest of the information ###
   ######################################################################
