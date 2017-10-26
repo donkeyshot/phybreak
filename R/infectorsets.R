0623ce6bb08b2d5bf4f04ece1c1c60597d300b71
donkeyshot <don@xs4all.nl>
2017-10-26 13:58:02 +0200

    Add possibility to return matrix. Let function work with new sample slot.

--- R/infectorsets.R
+++ R/infectorsets.R
@@ -1,20 +1,25 @@
 #' Sampled infectors for each host in a phybreak-object.
 #' 
 #' The function takes a \code{phybreak}-object containing MCMC-samples, and returns for each host 
-#'   a table with posterior infectors, with support per infector.
+#'   the posterior infectors, with support per infector. That can be in the form of a list, with infectors ordered
+#'   by support, or in the form of a matrix with supports for all host-infector combinations (host in columns, infectors in rows).
 #' 
 #' @param x An object of class \code{phybreak}.
-#' @param which.hosts A vector with hosts (positions in the dataset), or \code{"all"} for all hosts.
+#' @param which.hosts A vector with hosts (exact names or positions in the dataset), or \code{"all"} for all hosts.
 #' @param percentile Return infectors ordered by support, until a cumulative support indicated by \code{percentile},
 #'   support measured by proportion (value between 0 and 1).
-#' @param minsupport Only return infectors with more support than \code{minsupport}. Values in the range [0,1] are 
-#'   interpreted as support in \code{"proportion"} of posterior samples, values > 1 as code{"count"} of posterior 
+#' @param minsupport Only return infectors with more support than \code{minsupport}. Values in the range [0,1) are 
+#'   interpreted as support in \code{"proportion"} of posterior samples, values >= 1 as \code{"count"} of posterior 
 #'   samples.
 #' @param samplesize The number of samples to include (taken from the tail of the MCMC-chain).
 #' @param infector.name Whether to return the names of the infectors, or their position in the dataset.
 #' @param support Whether to return the support (= posterior probability) for each infector as a \code{"proportion"} 
 #'   or as a \code{"count"} of posterior trees in which that transmission link or transmission cluster is present.
-#' @return A named list with \code{data.frame}s, each with a vector of infectors and a vector of supports.
+#' @param output Whether to return a \code{list} of hosts with infectors, or a \code{matrix} with support values 
+#'   for all host-infector pairs.
+#'   Arguments \code{percentile}, \code{samplesize}, and \code{infector.name} are not used in case of \code{matrix}.
+#' @return A named list with \code{data.frame}s, each with a vector of infectors and a vector of supports. Or a matrix with
+#'   supports for each host-infector pair.
 #' @author Don Klinkenberg \email{don@@xs4all.nl}
 #' @references \href{http://dx.doi.org/10.1371/journal.pcbi.1005495}{Klinkenberg et al. (2017)} Simultaneous 
 #'   inference of phylogenetic and transmission trees in infectious disease outbreaks. 
@@ -29,51 +34,82 @@
 #' infectorsets(MCMCstate)
 #' @export
 infectorsets <- function(x, which.hosts = "all", percentile = 0.95, minsupport = 0, samplesize = Inf, infector.name = TRUE, 
-    support = c("proportion", "count")) {
-    ### initialize some constants
-    chainlength <- length(x$s$mu)
-    obs <- x$p$obs
+    support = c("proportion", "count"), output = c("list", "matrix")) {
+  if (!inherits(x, "phybreak")) 
+    stop("object should be of class \"phybreak\"")
+  
+  ### allow for older phybreak versions
+  obs <- x$p$obs
+  if(is.null(x$d$nsamples)) {
+    nsamples <- obs
+  } else {
+    nsamples <- x$d$nsamples
+  }
+  if(is.null(x$d$hostnames)) {
+    hostnames <- x$d$names
+  } else {
+    hostnames <- x$d$hostnames[1:obs]
+  }
+  if(is.null(x$s$infectors)) {
+    infectors <- x$s$nodehosts[nsamples:(nsamples + obs), ]
+  } else {
+    infectors <- x$s$infectors
+  }
+  
+  ### tests
+  if (!(is.character(which.hosts) | is.numeric(which.hosts)) | any(is.na(which.hosts))) {
+    stop("'which.hosts' should be numeric or \"all\", or should contain exact host names")
+  }
+  if(is.numeric(which.hosts)) {
+    which.hosts <- as.integer(which.hosts)
+    which.hosts <- unique(which.hosts)
+    which.hosts <- which.hosts[which.hosts >= 1 & which.hosts <= obs]
+  } else if (any(which.hosts == "all")) {
+    which.hosts <- 1:obs
+  } else {
+    which.hosts <- arg.match(which.hosts, hostnames, several.ok = TRUE)
+    which.hosts <- unique(which.hosts)
+    which.hosts <- match(which.hosts, hostnames)
+  } 
+  if (percentile < 0 | percentile > 1) {
+    stop("'percentile' should be given as number between 0 and 1")
+  }
+  if (minsupport < 0) {
+    minsupport <- 0
+    warning("'minsupport' is set to 0 (it should be >= 0). Values in the range [0,1] are interpreted as support in \"proportion\" of 
+        posterior samples, values > 1 as \"count\" of posterior samples" )
+  }
+  if (ncol(infectors) == 0) stop("no sampled trees available")
+  if (samplesize > ncol(infectors) & samplesize < Inf) {
+    warning("desired 'samplesize' larger than number of available samples")
+  }
+  support <- match.arg(support)
+  output <- match.arg(output)
+
+    ### samplesize
+    chainlength <- ncol(infectors)
     samplesize <- min(samplesize, chainlength)
     samplerange <- (chainlength - samplesize + 1):chainlength
-    if (support[1] == "count") {
+    if (support == "count") {
       denominator <- 1
     } else {
       denominator <- samplesize
     }
     
-    ### tests
-    if (!(is.character(which.hosts) | is.numeric(which.hosts)) | any(is.na(which.hosts))) {
-      stop("'which.hosts' should be numeric or \"all\", or should contain exact host names")
-    }
-    which.hosts <- unique(which.hosts)
-    if(is.numeric(which.hosts)) {
-      if(min(which.hosts) < 1 | max(which.hosts) > x$p$obs) {
-        stop("'which.hosts' contains out-of-range numbers, should be between 1 and outbreak size")
-      }
-    } else if (any(which.hosts == "all")) {
-      which.hosts <- 1:obs
-    } else if (!all(which.hosts %in% x$d$names)) {
-      stop("'which.hosts' contains non-existing host names'")
-    } else which.hosts <- match(which.hosts, x$d$hostnames)
-    if (percentile < 0 | percentile > 1) {
-      stop("'percentile' should be given as number between 0 and 1")
-    }
-    if (minsupport < 0) {
-      stop("'minsupport' should be >= 0. Values in the range [0,1] are interpreted as support in \"proportion\" of 
-        posterior samples, values > 1 as \"count\" of posterior samples" )
-    }
-    if (chainlength == 0) stop("no sampled trees available")
-    if (samplesize > chainlength & samplesize < Inf) {
-        warning("desired 'samplesize' larger than number of available samples")
-    }
-    if (support[1] != "proportion" && support[1] != "count") {
-        warning("support is given as proportion")
-    }
 
     ### obtaining the result in steps
     
     # array with ordered infectors per host, plus support
-    inffreqs <- .infarray(x$s$nodehosts[obs:(2 * obs - 1), samplerange])
+    
+    if(output == "matrix") {
+      supportmatrix <- apply(1 + infectors[, samplerange], 
+                             1, tabulate, nbin = 1 + obs)
+      colnames(supportmatrix) <- hostnames
+      rownames(supportmatrix) <- c("index", hostnames)
+      return(supportmatrix[, which.hosts]/denominator)
+    }
+    
+    inffreqs <- .infarray(infectors[, samplerange])
     
     # take out those with too little support
     if(minsupport < 1) {
@@ -90,7 +126,7 @@ infectorsets <- function(x, which.hosts = "all", percentile = 0.95, minsupport =
     if (infector.name) {
         res <- list()
         for (i in which.hosts) {
-            res <- c(res, list(data.frame(infector = c("index", x$d$names)[1 + inffreqs[i, 1, ][includemat[i, ]]], 
+            res <- c(res, list(data.frame(infector = c("index", hostnames)[1 + inffreqs[i, 1, ][includemat[i, ]]], 
                 support = inffreqs[i, 2, ][includemat[i, ]]/denominator)))
         }
     } else {
@@ -100,7 +136,7 @@ infectorsets <- function(x, which.hosts = "all", percentile = 0.95, minsupport =
                 ]]/denominator)))
         }
     }
-    names(res) <- x$d$hostnames[which.hosts]
+    names(res) <- hostnames[which.hosts]
     
     ### return the result
     return(res)
@@ -120,4 +156,5 @@ infectorsets <- function(x, which.hosts = "all", percentile = 0.95, minsupport =
 .inflist <- function(inf.chain, nhosts) {
     sapply(1:nhosts, .postinfector, inf.chain = inf.chain, support = TRUE)
 }
+
 
