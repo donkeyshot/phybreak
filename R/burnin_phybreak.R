ad98196e234749f96b9bb84b4624f1c74a53578e
Don Klinkenberg <don@xs4all.nl>
2022-05-03 11:44:58 +0200

    fix printing logLik during mcmc

--- R/burnin_phybreak.R
+++ R/burnin_phybreak.R
@@ -180,7 +180,7 @@ print_screen_log <- function(iteration) {
   if(pbe0$p$trans.model == "gamma"){
     message(paste0(
       stringr::str_pad(iteration, 8),
-      stringr::str_pad(round(sum(pbe0$logLikseq + pbe0$logLiksam + pbe0$logLikgen + pbe0$logLikcoal + pbe0$logLikdist + pbe0$logLikintro), 2), 12),
+      stringr::str_pad(round(sum(pbe0$logLikseq + pbe0$logLiksam + pbe0$logLikgen + pbe0$logLikcoal + pbe0$logLikdist), 2), 12),
       stringr::str_pad(signif(sum(pbe0$v$infectors==0), 1), 15),
       stringr::str_pad(signif(pbe0$p$mu, 3), 9),
       stringr::str_pad(signif(pbe0$p$gen.mean, 3), 10),
